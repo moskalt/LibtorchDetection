769b9bfed7398903c30f28d440740c3500c34439
moskalt <thetima2@gmail.com>
2021-04-13 14:34:42 +0700

    Build Configuration

--- src/detection.cpp
+++ src/detection.cpp
@@ -1,8 +1,14 @@
-#include "../include/detection.h"
 #include "torch/torch.h"
+#include "../v4l2lib/inc/V4l2ReadWriteDevice.h"
+#include "../v4l2lib/inc/V4l2MmapDevice.h"
+#include "../v4l2lib/inc/V4l2Output.h"
+#include "../v4l2lib/inc/V4l2Device.h"
+#include "../v4l2lib/inc/V4l2Access.h"
+#include "../v4l2lib/inc/V4l2Capture.h"
 
 
 int main(){
-    torch::Tensor tensor = torch::eye(3);
-    std::cout << tensor << std::endl;
+    V4L2DeviceParameters param("/dev/video0", V4L2_PIX_FMT_RGB24, 400, 400, 10, IOTYPE_MMAP, 0);
+    V4l2Capture* videoCapture = V4l2Capture::create(param);
+    std::cout << "YES" << std::endl;
 }
