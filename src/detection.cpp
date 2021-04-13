7e378c269826cfa4923216b897d95c6bb85a7cd7
moskalt <thetima2@gmail.com>
2021-04-13 19:26:06 +0700

    Working Video Capture

--- src/detection.cpp
+++ src/detection.cpp
@@ -1,14 +1,20 @@
-#include "torch/torch.h"
 #include "../v4l2lib/inc/V4l2ReadWriteDevice.h"
 #include "../v4l2lib/inc/V4l2MmapDevice.h"
 #include "../v4l2lib/inc/V4l2Output.h"
 #include "../v4l2lib/inc/V4l2Device.h"
 #include "../v4l2lib/inc/V4l2Access.h"
 #include "../v4l2lib/inc/V4l2Capture.h"
+#include <iostream>
+
 
 
 int main(){
-    V4L2DeviceParameters param("/dev/video0", V4L2_PIX_FMT_RGB24, 400, 400, 10, IOTYPE_MMAP, 0);
+    V4L2DeviceParameters param("/dev/video0", 0, 640, 480, 10, IOTYPE_MMAP, 0);
     V4l2Capture* videoCapture = V4l2Capture::create(param);
-    std::cout << "YES" << std::endl;
+    timeval timeout;
+    bool isReadable = (videoCapture->isReadable(&timeout) == 1);
+    char* buffer = new char[460800];
+    size_t nb = videoCapture->read(buffer,460800);
+    std::cin.get();
+    std::cin.ignore();
 }
